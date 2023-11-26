b4e0e745cf5c3a2c2bff2f69932f643bb10e2008
Brice Patchou <108960259+Brice12347@users.noreply.github.com>
2023-11-25 17:07:26 -0800

    changes to the marketplace
    silly little changes to marketplace

--- Trojan_Marketplace_Frontend/app/marketplaceguest/page.tsx
+++ Trojan_Marketplace_Frontend/app/marketplaceguest/page.tsx
@@ -8,22 +8,26 @@ const page = () => {
     {
       name: 'Shoes',
       description: 'If a dog chews shoes whose shoes does he choose?',
-      image: 'https://images.unsplash.com/photo-1612836674517-0f7f0b7d6a3a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXMlMjBzaG9vdHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+      image: 'https://images.unsplash.com/photo-1612836674517-0f7f0b7d6a3a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXMlMjBzaG9vdHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      alt: 'picture of shoes'
     },
     {
       name: 'skateboard',
-      description: 'If a dog chews shoes whose shoes does he choose?',
-      image: 'https://images.unsplash.com/photo-1612836674517-0f7f0b7d6a3a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXMlMjBzaG9vdHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+      description: ' Sam swiftly skates on a sleek skateboard, skillfully spinning through the city streets.',
+      image: 'https://images.unsplash.com/photo-1612836674517-0f7f0b7d6a3a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXMlMjBzaG9vdHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      alt: 'picture of skateboard'
     },
     {
       name: 'Cookie',
-      description: 'If a dog chews shoes whose shoes does he choose?',
-      image: 'https://images.unsplash.com/photo-1612836674517-0f7f0b7d6a3a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXMlMjBzaG9vdHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+      description: 'Craving crunchy cookies, Carla cheerfully chewed.',
+      image: 'https://images.unsplash.com/photo-1612836674517-0f7f0b7d6a3a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXMlMjBzaG9vdHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      alt: 'picture of Cookie'
     },
     {
       name: 'Notebook',
-      description: 'If a dog chews shoes whose shoes does he choose?',
-      image: 'https://images.unsplash.com/photo-1612836674517-0f7f0b7d6a3a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXMlMjBzaG9vdHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+      description: 'Naomis nimble fingers neatly noted in her notebook.',
+      image: 'https://images.unsplash.com/photo-1612836674517-0f7f0b7d6a3a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXMlMjBzaG9vdHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      alt: 'picture of Notebook'
     },
   ]
 
