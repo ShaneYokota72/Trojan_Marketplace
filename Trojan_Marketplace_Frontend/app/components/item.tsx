c72b8e43f56ba45aeec4ce58d50809555683c190
Brice Patchou <108960259+Brice12347@users.noreply.github.com>
2023-11-25 17:09:00 -0800

    update item.tsx

--- Trojan_Marketplace_Frontend/app/components/item.tsx
+++ Trojan_Marketplace_Frontend/app/components/item.tsx
@@ -5,7 +5,7 @@ export default function item( props :any ) {
   const {name, description, image} = props
   return (
     <div className="card w-72 bg-base-100 shadow-xl">
-        <figure><img src={image} alt="Shoes" /></figure>
+        <figure><img src={image} alt={name} /></figure>
         <div className="card-body">
             <h2 className="card-title">{name}</h2>
             <p>{description}</p>
